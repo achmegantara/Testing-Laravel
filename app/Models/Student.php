3f8d2e6474695cc9bfd467d3588e99e2937be33d
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-05-09 15:40:52 +0700

    add soft deletes

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Student extends Model
 {
     //diisi field mana saja yang boleh diisi
+    use SoftDeletes;
     protected $fillable = ['nama', 'nrp', 'email', 'jurusan'];
 
     //diisi fielad mana yang tidak boleh diisi
